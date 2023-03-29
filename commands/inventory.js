7e98b8a17443b2be38a70333341e7bf690bb58b9
greg stadermann <22124771+gregstadermann@users.noreply.github.com>
2023-03-29 18:06:00 -0400

    inventory work

--- commands/inventory.js
+++ commands/inventory.js
@@ -5,41 +5,40 @@ const ItemUtil = require('../../lib/lib/ItemUtil');
 
 module.exports = {
   usage: 'inventory',
-  command : (state) => (args, player) => {
-    if (!player.inventory || !player.inventory.size) {
-      return Broadcast.sayAt(player, "You aren't carrying anything.");
-    }
-
-    Broadcast.at(player, "You are carrying");
-    if (isFinite(player.inventory.getMax())) {
-      Broadcast.at(player, ` (${player.inventory.size}/${player.inventory.getMax()})`);
-    }
-    Broadcast.sayAt(player, ':');
-
-    // TODO: Implement grouping
-    const allPlayerItems = function() {
-      let equipmentTemp = [];
-      let inventoryTemp = [];
-
-      for (const [, item] of player.inventory) {
-        inventoryTemp.push(ItemUtil.display(item));
+  command : (state) => {
+    return (args, player) => {
+      if (!player.inventory || !player.inventory.size) {
+        return Broadcast.sayAt(player, "You aren't carrying anything.");
       }
-      for(const [slot, item ] of player.equipment)
-      {
-        equipmentTemp.push(ItemUtil.display(item));
+
+      /*
+          if (isFinite(player.inventory.getMax())) {
+            Broadcast.at(player, ` (${player.inventory.size}/${player.inventory.getMax()})`);
+          }
+      */
+
+      // TODO: Implement grouping
+      const allPlayerItems = function () {
+        let equipmentTemp = [];
+        let inventoryTemp = [];
+
+        for (const [, item] of player.inventory) {
+          inventoryTemp.push(ItemUtil.display(item));
+        }
+        for (const [slot, item] of player.equipment) {
+          equipmentTemp.push(ItemUtil.display(item));
+        }
+        Broadcast.sayAt(player, "You are wearing " + equipmentTemp + " " + "," + inventoryTemp);
+      };
+
+      if (!player.equipment.size) {
+        return Broadcast.sayAt(player, "You are completely naked!");
       }
-      Broadcast.sayAt(player, equipmentTemp+', '+inventoryTemp);
+      allPlayerItems();
+      Broadcast.sayAt(player, "You are wearing ");
+      for (const [slot, item] of player.equipment) {
+        Broadcast.sayAt(player, `  <${slot}> ${ItemUtil.display(item)}`);
+      }
     };
-
-
-
-    if (!player.equipment.size) {
-      return Broadcast.sayAt(player, "You are completely naked!");
-    }
-    allPlayerItems();
-    Broadcast.sayAt(player, "You are wearing ");
-    for (const [slot, item] of player.equipment) {
-      Broadcast.sayAt(player, `  <${slot}> ${ItemUtil.display(item)}`);
-    }
   }
 };
